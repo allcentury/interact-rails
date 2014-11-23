3ddab009af4fd2d589d4d2e7f305616e3a0ae58e
Anthony Ross <anthony.s.ross@gmail.com>
2014-11-23 15:53:58 -0500

    Clean up

--- lib/interact/rails.rb
+++ lib/interact/rails.rb
@@ -1,6 +1,7 @@
+require 'interact/rails/engine'
+require 'interact/rails/version'
+
 module Interact
   module Rails
-    class Engine < ::Rails::Engine
-    end
   end
 end
--- lib/rails.rb
+++
@@ -1,7 +0,0 @@
-require 'interact/rails/version'
-require 'interact/rails/rails'
-
-module Interact
-  module Rails
-  end
-end
