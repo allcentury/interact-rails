da9def9bcfa40f03c81e8f64dc19bbd7876c8900
Anthony Ross <anthony.s.ross@gmail.com>
2014-11-23 14:51:46 -0500

    Rails Engine
    Initial set up of rails engine add on

---
+++ lib/interact-rails.rb
@@ -0,0 +1,2 @@
+require 'interact/version'
+require 'interact/rails'
--- lib/interact/rails.rb
+++ lib/interact/rails.rb
@@ -2,6 +2,7 @@ require "interact/rails/version"
 
 module Interact
   module Rails
-    # Your code goes here...
+    class Engine < ::Rails::Engine
+    end
   end
 end
